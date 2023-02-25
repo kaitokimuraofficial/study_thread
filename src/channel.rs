074abe8e89328b7fb676c2567e6dc21b666292d1
kaitokimura <k_kimura@torotaku.jp>
2023-02-25 23:21:15 +0900

    fist commit

---
+++ src/channel.rs
@@ -0,0 +1,91 @@
+use std::thread;
+use std::sync::mpsc;
+use std::time::Duration;
+use std::sync::Mutex;
+
+pub fn run() {
+    // hi();
+    // multi_send();
+    //multi_send_2();
+    use_mutax();
+}
+
+fn hi() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
+
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
+}
+
+fn multi_send() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
+
+fn multi_send_2() {
+    let (tx, rx) = mpsc::channel();
+    let tx1 = mpsc::Sender::clone(&tx);
+    
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
+
+fn use_mutax() {
+    let m = Mutex::new(5);
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+    println!("{:?}",m);
+}
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+mod practice;
+mod channel;
+
+fn main() {
+    //practice::run();
+    channel::run();
+}
